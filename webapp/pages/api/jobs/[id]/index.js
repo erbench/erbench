0dc573ad97580b3b9d7308ac91a74aafacec6a54
Oleh Astappiev <4512729+astappiev@users.noreply.github.com>
2025-03-28 13:11:42 +0100

    feat(webapp): add slurm job ids

--- webapp/pages/api/jobs/[id]/index.js
+++ webapp/pages/api/jobs/[id]/index.js
@@ -28,5 +28,29 @@ export default async function handler(req, res) {
     }
   }
 
+  if (req.method === 'PUT') {
+    if (!req.body.status) {
+      return res.status(400).json({error: 'status is required'});
+    }
+
+    try {
+      const job = await prisma.job.update({
+        where: {
+          id: jobId,
+        },
+        data: {
+          status: req.body.status,
+          filteringSlurmId: req.body.filteringSlurmId,
+          matchingSlurmId: req.body.matchingSlurmId,
+        }
+      });
+
+      return res.status(200).json(job);
+    } catch (error) {
+      console.error("Error updating job:", error);
+      return res.status(500).json({error: "Failed to updating job"});
+    }
+  }
+
   return res.status(405).json({error: 'Method not allowed'});
 }
